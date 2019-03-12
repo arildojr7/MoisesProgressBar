a71ac78d0186296ac6382fd8f952df2d1ba4170a
arildo.junior <arildo.junior@4all.com>
2019-03-12 09:59:26 -0300

    Change resources

--- src/NyanIcons.java
+++ src/NyanIcons.java
@@ -3,6 +3,6 @@ import com.intellij.openapi.util.IconLoader;
 import javax.swing.*;
 
 public interface NyanIcons {
-    Icon CAT_ICON = IconLoader.getIcon("/rsz_cat.png");
-    Icon RCAT_ICON = IconLoader.getIcon("/rsz_rcat.png");
+    Icon VAI_MOISES = IconLoader.getIcon("/vai_moises.gif");
+    Icon VOLTA_MOISES = IconLoader.getIcon("/volta_moises.jpg");
 }
